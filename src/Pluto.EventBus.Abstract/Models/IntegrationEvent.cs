87fc49dd2db2c8faf926f79e1bed365560a07958
张玉龙 <620524a2924.cdb@sina.cn>
2022-03-14 20:38:42 +0800

    1

--- src/Pluto.EventBus.Abstract/Models/IntegrationEvent.cs
+++ src/Pluto.EventBus.Abstract/Models/IntegrationEvent.cs
@@ -29,6 +29,6 @@ namespace Pluto.EventBus.Abstract
         /// <summary>
         /// 延迟时间
         /// </summary>
-        public int DelaySecond { get; set; }
+        public long DelaySecond { get; set; }
     }
 }
